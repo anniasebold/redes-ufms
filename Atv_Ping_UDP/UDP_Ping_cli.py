e5dcb3f1ddd2a18ea6241a24dbeabab1ef3e31e0
anniasebold <anniasebold3.0@gmail.com>
2023-04-21 16:22:42 -0300

    udp_ping

--- Atv_Ping_UDP/UDP_Ping_cli.py
+++ Atv_Ping_UDP/UDP_Ping_cli.py
@@ -10,49 +10,47 @@ timeout = 1 # in second
 # Create UDP client socket
 # Note the use of SOCK_DGRAM for UDP datagram packet
 #/**/ /* 1. Cria o socket UDP */
+clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 # Set socket timeout as 1 second
 #/**/ /* Seta o timeout de 1s para o socket */
-
+clientSocket.settimeout(timeout)
 
 # Command line argument is a string, change the port into integer
-port = int(port)  
+port = int(port)
 # Sequence number of the ping message
-seqNum = 0  
+seqNum = 0
 
 # Ping for 10 times
-while seqNum < 10: 
-	seqNum += 1
-	# Format the message to be sent
-	data = "Ping " + str(seqNum) + " " + time.asctime()
+while seqNum < 10:
+  seqNum += 1
+  # Format the message to be sent
+  data = "Ping " + str(seqNum) + " " + time.asctime()
     
-	try:
-	# Sent time
-		RTTs = time.time()  #time in seconds
-	# Send the UDP packet with the ping message
-	#/**/ /* 2. Envia os dados para o servidor */
+  try:
+    # Sent time
+    RTTs = time.time()  #time in seconds
+    # Send the UDP packet with the ping message
+    #/**/ /* 2. Envia os dados para o servidor */
+    clientSocket.sendto(data.encode(), (host, port))
+    # Receive the server response
+    #/**/ /* 3. Recebe o reply do servidor */
+    message, address = clientSocket.recvfrom(1024)
 
-	# Receive the server response
-	#/**/ /* 3. Recebe o reply do servidor */
+    # Received time
+    RTTr = time.time()
+    # Display the server response as an output
+    print("Reply from " + address[0] + ": " + message.decode())       
+    # Round trip time is the difference between sent and received time
+    #/**/ /* 4. Apresenta o RTT na tela */
+    print("RTT: " + str(RTTr - RTTs))
 
-	# Received time
-		RTTr = time.time()
-	# Display the server response as an output
-		print("Reply from " + address[0] + ": " + message.decode())       
-	# Round trip time is the difference between sent and received time
-	#/**/ /* 4. Apresenta o RTT na tela */
-
-	except:
-		# Server does not respond
-	        # Assume the packet is lost
-		print ("Request", seqNum,"timed out.")
-		continue
+  except:
+    # Server does not respond
+    # Assume the packet is lost
+    print ("Request", seqNum,"timed out.")
+    continue
 
 # Close the client socket
 #/**/ /* 5. Fecha o socket */
-
- 
-
-
-
-
+clientSocket.close()
